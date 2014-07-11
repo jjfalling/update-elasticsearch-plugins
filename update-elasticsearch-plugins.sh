32060b545ae697d2c79b948fc55eb903ecda5762
Jeremy Falling <jjfalling@users.noreply.github.com>
2014-07-11 15:17:32 -0700

    Changed echos to printfs, fixed help message

--- update-elasticsearch-plugins.sh
+++ update-elasticsearch-plugins.sh
@@ -3,7 +3,7 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
 #****************************************************************************
 #*   update-elasticsearch-plugins                                           *
-#*   Update elasticsearch plugins that are hosted in git                    *
+#*   Update elasticsearch plugins that are hosted in github                 *
 #*                                                                          *
 #*   Copyright (C) 2014 by Jeremy Falling except where noted.               *
 #*                                                                          *
@@ -20,37 +20,40 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 #*   You should have received a copy of the GNU General Public License      *
 #*   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
 #****************************************************************************
-# We get list of plugins from elasticsearch: http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/modules-plugins.html
-# then compare it to a list of currently installed plugins.
 
-
-elasticsearchPluginPage='http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/modules-plugins.html'
+#Edit the following to match your needs
 elasticsearchPluginDir='/usr/share/elasticsearch/plugins'
 elasticsearchPlugin='/usr/share/elasticsearch/bin/plugin'
+elasticsearchPluginPage='http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/modules-plugins.html'
 
 #define a hash with the plugin name as the key and github user/plugin name as the value
 declare -A customPlugins
 customPlugins=( ["kopf"]="lmenezes/elasticsearch-kopf" )
 
+
+#End of options
+#############################################################################
+
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
-    echo "This script needs to run as root to run the updates!" 1>&2
+    printf "This script needs to run as root to run the updates!\n" 1>&2
     exit 1
 fi
 
 if [[ "$1"  == "-h" ]]
 then
-    echo "update-elasticsearch-plugins"
-    echo "This will update your elasticsearch plugins in interactive mode"
-    echo "or automaticly if you give the -y flag"
-    echo "You can also manually add plugins hosted on github that are not on the es plugin page by editing the customPlugins hash"
+    printf "Usage: update-elasticsearch-plugins [-y]\n\n"
+    printf "This will update your elasticsearch plugins in one of two modes, interactive or automatic\n"
+    printf "Interactive mode will ask before upgrading each plugin, skipping the plugin if you do not want to upgrade it.\n"
+    printf "Automatic mode upgrade everything without asking if you give the -y flag\n\n"
+
+    printf "Normally your plugin needs to be listed on the elasticsearch plugin page. However you can also manually add plugins hosted on github by editing the customPlugins hash in this script\n\n"
     exit
 
 fi
 
 
 printf "\n\nupdate-elasticsearch-plugins\n"
-printf "This will offer to update github hosted plugins. It will remove the old version before installing the new one.\n"
 
 auto=0
 if [[ "$1"  == "-y" ]]
@@ -63,7 +66,7 @@ fi
 #check if curl is installed
 command -v curl >/dev/null 2>&1 || { printf "\nERROR: curl is not installed\n\n"; exit 1; }
 
-echo "Obtaining current list of plugins, please wait..."
+printf "Obtaining current list of plugins from the elasticsearch website, please wait...\n"
 
 #curl the es plugin page. Then grep for each url line (contains ulink), remove a bogus line, strip it down to just the url, then shove it in an array
 esPlugins=(`curl -s $elasticsearchPluginPage | grep ulink | grep -v "be found under the" |sed 's/<a class="ulink" href="//' | sed 's/" target=".*//'`)
@@ -72,7 +75,7 @@ esPlugins=(`curl -s $elasticsearchPluginPage | grep ulink | grep -v "be found un
 installedPlugins=(`ls $elasticsearchPluginDir |grep -v preupgrade*`)
 
 #look at each installed plugin and try to find its repo, then offer to update
-echo "Looking at plugins"
+printf "Looking at plugins\n"
 for currentInstalledPlugin in "${installedPlugins[@]}"
 do
 
@@ -89,7 +92,7 @@ do
 
         if [ $response == "y" ]
         then
-            echo "using custom plugin url instead of offical es plugin url for $currentInstalledPlugin"
+            printf "using custom plugin url instead of offical es plugin url for $currentInstalledPlugin\n"
             $elasticsearchPlugin -r $currentInstalledPlugin
             $elasticsearchPlugin -i ${customPlugins["$currentInstalledPlugin"]}
         fi
@@ -121,7 +124,8 @@ do
                 break
             fi
 
-	done
+        done
     fi
 done
 
+printf "\nDone.\n"
